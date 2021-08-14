0c5678b03cf37b6841d649b24d22873e2e986e15
faithdinno <adebayooluwadara@gmail.com>
2021-08-15 00:27:36 +0100

    updates

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->integerIncrements('id');
             $table->string('name');
             $table->string('email', 225)->unique();
             $table->string('unique', 225)->unique();
